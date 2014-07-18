369fe9c3d56369f1e8ef83a46eca9b8808d965c3
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-07-17 23:57:50 -0700

    More filters in spotify and get track_meta_data

--- spotify.py
+++ spotify.py
@@ -1,5 +1,9 @@
 import re
 import secret
+import json
+import spotipy
+
+
 def filter_keywords_from_title(title):
     keywords = [  r"(\(\s*official video\s*\))",
                   r"(\[\s*official video\s*\])",
@@ -20,12 +24,22 @@ def filter_keywords_from_title(title):
                   r"(\[\s*explicit\s*\])",
                   r"(\(\s*official\s*\))",
                   r"(\[\s*official\s*\])",
+                  r"(\(\s*audio\s*\))",
+                  r"(\[\s*audio\s*\])",
                   r"(-)",
                     ]
     p = re.compile('|'.join(keywords), re.IGNORECASE)
     while p.subn(' ', title)[1] > 0:
       title = p.subn(' ', title)[0].strip()
     return title
+
+def get_track_meta_data(track_name):
+  result = {}
+  sp = spotipy.Spotify()
+  search_result = sp.search(track_name, limit = 1)['tracks']['items'][0]
+
+
+
 
 if __name__ == '__main__':
   print filter_keywords_from_title("David Guetta - She Wolf (Falling To Pieces) ft. Sia (Official Video)")
