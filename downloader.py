cd85d97e66acbfcbdd446fe4fec2d46c14d6095f
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-07-18 01:01:43 -0700

    Download to /tmp/music

--- downloader.py
+++ downloader.py
@@ -4,7 +4,7 @@ youtube_url = "https://www.youtube.com/watch?v="
 
 def download_video(video_id):
   url = youtube_url + video_id
-  p = subprocess.Popen(["youtube-dl", "-f", "bestaudio", "-o", "/tmp/%(title)s.%(ext)s", url], stdout=subprocess.PIPE)
+  p = subprocess.Popen(["youtube-dl", "-f", "bestaudio", "-o", "/tmp/music/%(title)s.%(ext)s", url], stdout=subprocess.PIPE)
   result = p.communicate()[0]
 
   if p.returncode == 0:
@@ -15,9 +15,9 @@ def download_video(video_id):
         return line.split('[download]')[1].split('has already been downloaded')[0].strip()
 
 def mp3_encode(m4a_file):
-  mp4_file = m4a_file.replace(".m4a", ".mp3")
-  p = subprocess.call(["ffmpeg", "-i", m4a_file, mp4_file])
-  return mp4_file
+  mp3_file = m4a_file.replace(".m4a", ".mp3")
+  p = subprocess.call(["ffmpeg", "-i", m4a_file, mp3_file])
+  return mp3_file
 
 
 if __name__ == '__main__':
