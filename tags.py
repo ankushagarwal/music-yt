287cd2d859eaab9b878c22926153b42d40e53b89
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-07-18 01:02:31 -0700

    Everybody gets tags!!

--- tags.py
+++ tags.py
@@ -1,8 +1,10 @@
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC, error
+from mutagen.easyid3 import EasyID3
+
 import tempfile
 import requests
-
+import spotify
 def download_album_art(album_art_url):
   f = tempfile.NamedTemporaryFile(delete=False)
   r = requests.get(album_art_url)
@@ -31,6 +33,19 @@ def add_album_art(file_name, album_art_url):
   )
   audio.save()
 
+def add_all_tags(mp3_file):
+  track_name = mp3_file.split('/')[-1]
+  track_name = track_name.replace('.mp3', '')
+  result = spotify.get_track_meta_data(track_name)
+  album_data = spotify.get_album(result['album']['id'])
+  audio = EasyID3(mp3_file)
+  audio["title"] = result['name']
+  audio["album"] = result['album']['name']
+  audio["artist"] = album_data['artists'][0]['name']
+  #audio["year"] = album_data["release_date"].split("-")[0]
+  audio.save()
+  add_album_art(mp3_file, album_data['images'][0]['url'])
 
 if __name__ == '__main__':
-  add_album_art('/tmp/Pitbull - International Love ft. Chris Brown.mp3', 'https://i.scdn.co/image/d962669f8cb25b4d41b5f8970960d819ea88e2fc')
+  add_all_tags('/tmp/Pitbull - International Love.mp3')
+  #add_album_art('/tmp/Pitbull - International Love ft. Chris Brown.mp3', 'https://i.scdn.co/image/d962669f8cb25b4d41b5f8970960d819ea88e2fc')
